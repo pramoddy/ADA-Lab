bf556a11eb5a203a7c99125dd008daad2fa4a227
pramoddy <55015217+pramoddy@users.noreply.github.com>
2020-02-11 13:18:49 +0530

    new added

---
+++ bse.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+   int a[40],i,n,mid,high,low=0,key,p=0;
+   printf("Enter the how many value to store\n");
+   scanf("%d",&n);
+   printf("Enter the key element\n");
+   scanf("%d",&key);
+   printf("Enter the %d values\n",n);
+   for(i=0;i<n;i++)
+   {
+         scanf("%d",&a[i]);
+    }
+    
+      high=n;
+      low=0; 
+     while(1)
+  {
+      mid=(low+high)/2;
+      if(a[mid]<key){
+                        high=mid+1;
+      }
+      else if(a[mid]>key){
+                        high=mid-1;
+      }
+      else if(a[mid]==key){
+                        printf("success.......\n");   
+                        for(i=0;i<n;i++)
+                        {
+                          
+                           if(key==a[i])
+                           {
+                              p=p+1;
+                            }
+                           
+                         }
+                            printf("The %d key element is exist %d times\n",key,p);
+                            printf("first %d and last element is %d\n",a[0],a[n-1]);
+                        exit(0);
+                        }
+      
+      printf("Unsuccesfull!!.......\n");
+      exit(0);
+  }   
+        
+        
+  }      
+        
+        
